a574deafccfc0b9cd7660aed09adce3d9ee5a13f
kevin <KevinS9sotob@hotmail.com>
2025-06-05 15:58:25 +0200

    frontend Inicio

---
+++ shopradar/src/components/ContenidoInicio.tsx
@@ -0,0 +1,67 @@
+import Leche from "./icons/Leche";
+import Frutas from "./icons/Frutas";
+import Pan from "./icons/Pan";
+import Carne from "./icons/Carne";
+
+function ContenidoInicio() {
+  return (
+    <section className="max-w-4xl mx-auto px-4 flex flex-col justify-center items-center text-center mb-26">
+                <h2 className="text-4xl font-bold text-gray-800 mb-6 font-Zodiak">
+                    ¿Qué producto buscas hoy?
+                </h2>
+                <p className="text-gray-600 mb-4 text-xl font-Switzer">
+                    Encuentra los mejores precios en los supermercados más populares
+                    de España.
+                </p>
+                <div className="grid grid-cols-1 sm:grid-cols-4 gap-6 mt-10 font-Switzer w-full">
+                    <span className="flex flex-col items-center justify-center gap-2 bg-blue-500 px-18 py-8 rounded-xl">
+                        <Leche/>
+                        <p>
+                            Lácteos
+                        </p>
+                    </span>
+                    <span className="flex flex-col items-center justify-center gap-2 bg-green-500 px-18 py-8 rounded-xl">
+                        <Frutas/>
+                        <p>
+                            Frutas
+                        </p>
+                    </span >
+                    <span className="flex flex-col items-center justify-center gap-2 bg-orange-400 px-18 py-8 rounded-xl">
+                        <Pan/>
+                        <p>
+                            Panadería
+                        </p>
+                    </span>
+                    <span className="flex flex-col items-center justify-center gap-2 bg-red-500 px-18 py-8 rounded-xl">
+                        <Carne/>
+                        <p>
+                            Carnes
+                        </p>
+                    </span>
+                </div>
+
+                <div className="flex md:flex-row flex-col md:gap-30 gap-10 items-center justify-evenly shadow-lg backdrop-blur-md rounded-2xl p-6 mt-10 w-full font-Switzer">
+                    <span className="text-blue-500 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
+                        50 +
+                        <p className="text-gray-700 text-base font-normal">
+                            Supermercados 
+                        </p>
+                    </span>
+                    <span className="text-green-500 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
+                        10K +
+                        <p className="text-gray-700 text-base font-normal">
+                            Productos
+                        </p>
+                    </span>
+                    <span className="text-orange-400 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
+                        30 %
+                        <p className="text-gray-700 text-base font-normal">
+                            Ahorro Promedio
+                        </p>
+                    </span>
+                </div>
+            </section>
+  );
+}
+
+export default ContenidoInicio;
--- shopradar/src/pages/Inicio.tsx
+++ shopradar/src/pages/Inicio.tsx
@@ -2,10 +2,7 @@ import ContactoIcon from "../components/icons/Contacto";
 import DescuentoIcon from "../components/icons/DescuentoIcon";
 import SupermercadoIcon from "../components/icons/SupermercadoIcon";
 import Buscador from "../components/Buscador";
-import Leche from "../components/icons/Leche";
-import Frutas from "../components/icons/Frutas";
-import Pan from "../components/icons/Pan";
-import Carne from "../components/icons/Carne";
+import ContenidoInicio from "../components/ContenidoInicio";
 
 function Inicio() {
     return (
@@ -48,63 +45,7 @@ function Inicio() {
                 </div>
             </section>
 
-            <section className="max-w-4xl mx-auto px-4 flex flex-col justify-center items-center text-center mb-26">
-                <h2 className="text-4xl font-bold text-gray-800 mb-6 font-Zodiak">
-                    ¿Qué producto buscas hoy?
-                </h2>
-                <p className="text-gray-600 mb-4 text-xl font-Switzer">
-                    Encuentra los mejores precios en los supermercados más populares
-                    de España.
-                </p>
-                <div className="grid grid-cols-1 sm:grid-cols-4 gap-6 mt-10 font-Switzer w-full">
-                    <span className="flex flex-col items-center justify-center gap-2 bg-blue-500 px-18 py-8 rounded-xl">
-                        <Leche/>
-                        <p>
-                            Lácteos
-                        </p>
-                    </span>
-                    <span className="flex flex-col items-center justify-center gap-2 bg-green-500 px-18 py-8 rounded-xl">
-                        <Frutas/>
-                        <p>
-                            Frutas
-                        </p>
-                    </span >
-                    <span className="flex flex-col items-center justify-center gap-2 bg-orange-400 px-18 py-8 rounded-xl">
-                        <Pan/>
-                        <p>
-                            Panadería
-                        </p>
-                    </span>
-                    <span className="flex flex-col items-center justify-center gap-2 bg-red-500 px-18 py-8 rounded-xl">
-                        <Carne/>
-                        <p>
-                            Carnes
-                        </p>
-                    </span>
-                </div>
-
-                <div className="flex md:flex-row flex-col md:gap-30 gap-10 items-center justify-evenly shadow-lg backdrop-blur-md rounded-2xl p-6 mt-10 w-full font-Switzer">
-                    <span className="text-blue-500 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
-                        50 +
-                        <p className="text-gray-700 text-base font-normal">
-                            Supermercados 
-                        </p>
-                    </span>
-                    <span className="text-green-500 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
-                        10K +
-                        <p className="text-gray-700 text-base font-normal">
-                            Productos
-                        </p>
-                    </span>
-                    <span className="text-orange-400 text-3xl flex flex-col items-center justify-center gap-2 font-bold">
-                        30 %
-                        <p className="text-gray-700 text-base font-normal">
-                            Ahorro Promedio
-                        </p>
-                    </span>
-                </div>
-            </section>
-
+            <ContenidoInicio />
         </>
     );
 }

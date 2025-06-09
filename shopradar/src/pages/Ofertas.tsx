c640605a748b4be769335727411e802be2ad7d8d
kevin <KevinS9sotob@hotmail.com>
2025-06-09 19:20:14 +0200

    fix

--- shopradar/src/pages/Ofertas.tsx
+++ shopradar/src/pages/Ofertas.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ResultadosBusqueda from '../components/ResultadosBusqueda';
 import { getFilteredOffers } from '../services/searchProducts';
 import type { ProductWithSupermarket } from '../services/searchProducts';
